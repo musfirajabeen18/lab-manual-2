1cf4e66cc64bcfe95d6775af8282599c7158e0be
musfirajabeen18 <musfirajabeen74@gmail.com>
2026-03-08 20:44:47 +0500

    Add files via upload

---
+++ task3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+class Calculator
+{
+    int num1;
+    int num2;
+    public Calculator(int a, int b)
+    {
+        num1 = a;
+        num2 = b;
+    }
+    public void Add()
+    {
+        Console.WriteLine("Addition: " + (num1 + num2));
+    }
+    public void Subtract()
+    {
+        Console.WriteLine("Subtraction: " + (num1 - num2));
+    }
+    public void Multiply()
+    {
+        Console.WriteLine("Multiplication: " + (num1 * num2));
+    }
+    public void Divide()
+    {
+        Console.WriteLine("Division: " + (num1 / num2));
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        Calculator c = new Calculator(10, 5);
+        c.Add();
+        c.Subtract();
+        c.Multiply();
+        c.Divide();
+    }
+}
